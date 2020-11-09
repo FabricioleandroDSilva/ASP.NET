fee80fb791bc16dd98702998875ad9de3c8617c3
Fabricio Leandro Da Silva <fabricioleandrosilva@gmail.com>
2020-11-09 20:37:27 -0300

    Ficha Academia

---
+++ FichaAcademia/FichaAcademia.AcessoDados/Contexto.cs
@@ -0,0 +1,39 @@
+﻿using FichaAcademia.AcessoDados.Mapeamentos;
+using FichaAcademia.Dominio.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FichaAcademia.AcessoDados
+{
+   public class Contexto : DbContext
+    {
+        public DbSet<Administrador> Administradores { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<CategoriaExercicio> CategoriaExercicios { get; set; }
+        public DbSet<Exercicio> Exercicios { get; set; }
+        public DbSet<Ficha> Fichas { get; set; }
+        public DbSet<ListaExercicio> ListaExercicios { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Objetivo> Objetivos { get; set; }
+        public Contexto(DbContextOptions<Contexto> opcoes):base (opcoes)
+        {
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AdministradoresMap());
+            modelBuilder.ApplyConfiguration(new AlunoMap());
+            modelBuilder.ApplyConfiguration(new CategoriaExericioMap());
+            modelBuilder.ApplyConfiguration(new ExericioMap());
+            modelBuilder.ApplyConfiguration(new FichaMap());
+            modelBuilder.ApplyConfiguration(new ListaExercicioMap());
+            modelBuilder.ApplyConfiguration(new ObjetivoMap());
+            modelBuilder.ApplyConfiguration(new ProfessorMap());
+
+        }
+    }
+}
---
+++ FichaAcademia/FichaAcademia.AcessoDados/Mapeamentos/CategoriaExericioMap.cs
@@ -0,0 +1,24 @@
+﻿using FichaAcademia.Dominio.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FichaAcademia.AcessoDados.Mapeamentos
+{
+    public class CategoriaExericioMap : IEntityTypeConfiguration<CategoriaExercicio>
+    {
+        public void Configure(EntityTypeBuilder<CategoriaExercicio> builder)
+        {
+            builder.HasKey(c => c.CategoriaExercicioId);
+
+            builder.Property(c => c.Nome).IsRequired();
+
+
+            builder.HasMany(c => c.Exercicio).WithOne(c=> c.CategoriaExercicio).OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable("CategoriasExericios");
+        }
+    }
+}
