08d622996e66c09463b988deeb992cfda3739b32
Fabricio Leandro Da Silva <fabricioleandrosilva@gmail.com>
2021-01-04 08:44:53 -0300

    Teste Cadasdro de Clientes

---
+++ CadastroClientes/CadastroClientes/Mapeamento/ClienteMap.cs
@@ -0,0 +1,23 @@
+﻿using CadastroCliente.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCliente.Mapeamento
+{
+    public class ClienteMap : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.Property(c => c.Nome).IsRequired();
+            builder.Property(c => c.DataNascimento).IsRequired();
+            builder.Property(c => c.Sexo).IsRequired();
+
+            builder.ToTable("Clientes");
+
+        }
+    }
+}
---
+++ CadastroClientes/CadastroClientes/Models/Contexto.cs
@@ -0,0 +1,28 @@
+﻿using CadastroCliente.Mapeamento;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCliente.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           
+            builder.ApplyConfiguration(new ClienteMap());
+
+
+
+        }
+
+    }
+}
